ff52bdc0b49e2b325adab6caede3266483cd1f8c
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-10-25 22:57:00 -0300

    inicio do projeto

---
+++ src/main/java/br/com/nativos/produtivos/ProdutivosApplication.java
@@ -0,0 +1,13 @@
+package br.com.nativos.produtivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nativos/produtivos/ProdutivosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nativos.produtivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
