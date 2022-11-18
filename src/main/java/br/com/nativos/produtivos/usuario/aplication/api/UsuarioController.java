6a9d8386c6d597e0f7b541eec3931bd8b473bf84
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-11-17 23:05:16 -0300

    Injeta dependecia UsuarioAplicatonService e  acrescenta @RequeredArgsConstructor na clase UsuarioController

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioController.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioController.java
@@ -1,5 +1,8 @@
 package br.com.nativos.produtivos.usuario.aplication.api;
 
+import br.com.nativos.produtivos.usuario.aplication.service.UsuarioAplicationService;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +11,15 @@ import java.util.UUID;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
+    private final UsuarioAplicationService usuarioAplicationService;
 
     @Override
     public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
        log.info("[inicio] UsuarioController - postNovoUsuario");
-     //  UsuarioCriadoResponse usuarioCriado = postNovoUsuario.criaNovoUsuarioPorId(IdUsuario);
-      // log.info("[final] UsuarioController - postNovoUsuario");
-        return null;
+       UsuarioCriadoResponse usuarioCriado = usuarioAplicationService.criaNovoUsuario(usuarioNovo);
+       log.info("[final] UsuarioController - postNovoUsuario");
+       return usuarioCriado;
     }
 }
