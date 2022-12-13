2511ea96ce97c3fbc9a9c6f942263f3b50512272
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-12 23:32:08 -0300

    altera UsuarioAplicationService de inteface para classe, fazendo com que fique mais organizado as nomeclaturas e implementa os metodos criaNovoUsuario e deletaUsuarioUsandoId

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioAplicationService.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioAplicationService.java
@@ -2,7 +2,34 @@ package br.com.nativos.produtivos.usuario.aplication.service;
 
 import br.com.nativos.produtivos.usuario.aplication.api.UsuarioCriadoResponse;
 import br.com.nativos.produtivos.usuario.aplication.api.UsuarioNovoRequest;
+import br.com.nativos.produtivos.usuario.domain.Usuario;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
-public interface UsuarioAplicationService {
-    UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
+import java.util.UUID;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class UsuarioAplicationService implements UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo) {
+        log.info("[inicia] UsuarioServicee - criaNovoUsuario");
+        Usuario usuario = usuarioRepository.salva(new Usuario(usuarioNovo));
+        log.info("[finaliza] UsuarioService - criaNovoUsuario");
+        return UsuarioCriadoResponse.builder()
+                .idUsuario(usuario.getIdUsuario())
+                .build();
+    }
+
+    @Override
+    public void deletaUsuarioUsandoId(UUID idUsuario) {
+        log.info("[inicia] UsuarioServicee - criaNovoUsuario");
+
+        log.info("[finaliza] UsuarioService - criaNovoUsuario");
+
+    }
 }
