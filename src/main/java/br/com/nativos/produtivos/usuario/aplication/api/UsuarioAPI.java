de5a6d77111b93def723726bd23f034e2fb3465e
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-12 23:34:17 -0300

    implementa o metodo deletaUsuarioPeloID e altera a nomeclatura do metodo postNovoUsuario

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioAPI.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioAPI.java
@@ -4,11 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/v1/usuario")
 public interface UsuarioAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    UsuarioCriadoResponse postNovoUsuario (@RequestBody @Valid UsuarioNovoRequest usuarioNovo);
+    UsuarioCriadoResponse criaNovoUsuario(@Valid @RequestBody UsuarioNovoRequest usuarioNovo);
+
+    @DeleteMapping (value = "/{idUsuario}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    void deletaUsuarioPeloID(@PathVariable UUID idUsuario);
+
+//    List<UsuarioCriadoResponse> listaTodosUsuarios(@Valid @RequestBody UsuarioNovoRequest usuarioNovo);
 }
