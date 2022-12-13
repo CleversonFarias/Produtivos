9ddd5291a8dd4f04ff907848f74fc79e48303abb
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-12 23:28:45 -0300

    implementa metodo deletaUsuarioPeloID, altera nomeclatura postNovoUsuario para criaNovoUsuario e altera nomeclatura do atributo da injecao de dependecia

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioController.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioController.java
@@ -1,25 +1,31 @@
 package br.com.nativos.produtivos.usuario.aplication.api;
 
-import br.com.nativos.produtivos.usuario.aplication.service.UsuarioAplicationService;
-import lombok.Builder;
+import br.com.nativos.produtivos.usuario.aplication.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
 @Log4j2
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
-    private final UsuarioAplicationService usuarioAplicationService;
+    private final UsuarioService usuarioService;
 
     @Override
-    public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
+    public UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo) {
        log.info("[inicio] UsuarioController - postNovoUsuario");
-       UsuarioCriadoResponse usuarioCriado = usuarioAplicationService.criaNovoUsuario(usuarioNovo);
+       UsuarioCriadoResponse usuarioCriado = usuarioService.criaNovoUsuario(usuarioNovo);
        log.info("[final] UsuarioController - postNovoUsuario");
        return usuarioCriado;
+    }
+
+    @Override
+    public void deletaUsuarioPeloID(UUID idUsuario) {
+        log.info("[inicio] UsuarioController - deletaUsuarioPeloID");
+        usuarioService.deletaUsuarioUsandoId(idUsuario);
+        log.info("[final] UsuarioController - deletaUsuarioPeloID");
+
     }
 }
