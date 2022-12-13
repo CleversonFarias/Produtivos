85cb73b334b6e2f3229e683cdba82a7ce67cdaf3
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-12 23:24:32 -0300

    Altera UsuarioService de classe para interface para ficar mais semantico o codigo e implementa o metodo deletaUsuarioUsandoId

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioService.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioService.java
@@ -2,18 +2,10 @@ package br.com.nativos.produtivos.usuario.aplication.service;
 
 import br.com.nativos.produtivos.usuario.aplication.api.UsuarioCriadoResponse;
 import br.com.nativos.produtivos.usuario.aplication.api.UsuarioNovoRequest;
-import br.com.nativos.produtivos.usuario.domain.Usuario;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Service;
 
-@Service
-@Log4j2
-public class UsuarioService implements UsuarioAplicationService {
-    @Override
-    public UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo) {
-        log.info("[inicio] UsuarioService - criaNovoUsuario");
-        Usuario usuario = new Usuario(usuarioNovo);
-        log.info("[final] UsuarioService - criaNovoUsuario");
-        return new UsuarioCriadoResponse(usuario);
-    }
+import java.util.UUID;
+
+public interface UsuarioService {
+    UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
+    void deletaUsuarioUsandoId(UUID idUsuario);
 }
