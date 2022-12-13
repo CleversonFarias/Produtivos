9851d607b5192f0d9e79a2fd44e4e1f5720731fe
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-12 23:11:45 -0300

    Atualiza anotacoes de mongoDB para mySQL

--- src/main/java/br/com/nativos/produtivos/projeto/domain/Projeto.java
+++ src/main/java/br/com/nativos/produtivos/projeto/domain/Projeto.java
@@ -4,21 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Document(collection = "Projeto")
+@Entity
 public class Projeto {
    @Id
    private UUID idProjeto;
-   @Indexed
    private UUID idUsuario;
    private String nome;
    private String descricao;
