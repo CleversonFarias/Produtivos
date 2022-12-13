f14bc8bc9c76fdf8c9fb77cc88c1c25932d6aaf5
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-12 23:17:50 -0300

    incrementa anotacao builder e exclui excesso de atributos

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioCriadoResponse.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/api/UsuarioCriadoResponse.java
@@ -1,19 +1,16 @@
 package br.com.nativos.produtivos.usuario.aplication.api;
 
-import br.com.nativos.produtivos.usuario.domain.Usuario;
+import lombok.Builder;
 import lombok.Value;
 
 import java.util.UUID;
+@Builder
 @Value
 public class UsuarioCriadoResponse {
-    private final UUID idUsuario;
-    private final String nome;
-    private final String email;
+    private UUID idUsuario;
 
-
-    public UsuarioCriadoResponse(Usuario usuario) {
-        this.idUsuario = usuario.getIdUsuario();
-        this.email = usuario.getEmail();
-        this.nome = usuario.getNome();
-    }
+//    public UsuarioCriadoResponse(Usuario usuario) {
+//        this.idUsuario = usuario.getIdUsuario();
+//        this.email = usuario.getEmail();
+//    }
 }
