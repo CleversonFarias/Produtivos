a1a755ba35b9b37c618b060df6f8e66c696da071
Cleverson Farias <cleverson_sairaf@outlook.com>
2022-12-17 12:54:19 -0300

    implementa o metodo  deleta

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioRepository.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioRepository.java
@@ -1,7 +1,12 @@
 package br.com.nativos.produtivos.usuario.aplication.service;
 
+import br.com.nativos.produtivos.usuario.aplication.api.UsuarioCriadoResponse;
 import br.com.nativos.produtivos.usuario.domain.Usuario;
+
+import java.util.UUID;
 
 public interface UsuarioRepository {
     Usuario salva(Usuario usuario);
+    void deleta(UUID idUsuario);
+    UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario);
 }
