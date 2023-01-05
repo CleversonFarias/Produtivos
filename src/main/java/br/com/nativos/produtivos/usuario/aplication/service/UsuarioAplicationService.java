4c12ad293e1ab99197ba044aabb177b40e0f02d1
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-01-05 17:30:20 -0300

    implementa o metodo  buscaTodosUsuario, altera metodo buscaUsuarioPorId e implementa metodo alteraUsuarioPeloId

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioAplicationService.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioAplicationService.java
@@ -1,12 +1,12 @@
 package br.com.nativos.produtivos.usuario.aplication.service;
 
-import br.com.nativos.produtivos.usuario.aplication.api.UsuarioCriadoResponse;
-import br.com.nativos.produtivos.usuario.aplication.api.UsuarioNovoRequest;
+import br.com.nativos.produtivos.usuario.aplication.api.*;
 import br.com.nativos.produtivos.usuario.domain.Usuario;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,26 +17,41 @@ public class UsuarioAplicationService implements UsuarioService {
 
     @Override
     public UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo) {
-        log.info("[inicia] UsuarioServicee - criaNovoUsuario");
+        log.info("[inicia] UsuarioAplicationService - criaNovoUsuario");
         Usuario usuario = usuarioRepository.salva(new Usuario(usuarioNovo));
-        log.info("[finaliza] UsuarioService - criaNovoUsuario");
+        log.info("[final] UsuarioAplicationService - criaNovoUsuario");
         return UsuarioCriadoResponse.builder()
                 .idUsuario(usuario.getIdUsuario())
                 .build();
     }
 
     @Override
-    public UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario) {
-        log.info("[inicia] UsuarioServicee - deletaUsuarioUsandoId");
-        UsuarioCriadoResponse buscaUsuario = usuarioRepository.buscaUsuarioPorId(idUsuario);
-        log.info("[finaliza] UsuarioService - deletaUsuarioUsandoId");
-        return buscaUsuario;
+    public UsuarioCriadoDetalhado buscaUsuarioPorId(UUID idUsuario) {
+        log.info("[inicia] UsuarioAplicationService - buscaUsuarioPorId");
+        Usuario usuario = usuarioRepository.buscaUsuarioPorId(idUsuario);
+        log.info("[final] UsuarioAplicationService - buscaUsuarioPorId");
+        return new UsuarioCriadoDetalhado(usuario);
+    }
+    @Override
+    public List<UsuarioListResponse> buscaTodosUsuarios() {
+        log.info("[inicia] UsuarioAplicationService - buscaTodosUsuarios");
+        List<Usuario> Usuarios = usuarioRepository.buscaTodosUsuarios();
+        log.info("[final] UsuarioAplicationService - buscaTodosUsuarios");
+        return UsuarioListResponse.converte(Usuarios);
     }
 
     @Override
     public void deletaUsuarioUsandoId(UUID idUsuario) {
-        log.info("[inicia] UsuarioServicee - deletaUsuarioUsandoId");
+        log.info("[inicia] UsuarioAplicationService - deletaUsuarioUsandoId");
         usuarioRepository.deleta(idUsuario);
-        log.info("[finaliza] UsuarioService - deletaUsuarioUsandoId");
+        log.info("[final] UsuarioAplicationService - deletaUsuarioUsandoId");
+    }
+    @Override
+    public void alteraUsuarioPeloId(UUID idUsuario, UsuarioAlteracaoRequest usuarioAlteracaoRequest) {
+        log.info("[inicia] UsuarioAplicationService - alteraUsuarioPeloId");
+        Usuario usuario = usuarioRepository.buscaUsuarioPorId(idUsuario);
+        usuario.alteraUsuarioPeloId(usuarioAlteracaoRequest);
+        usuarioRepository.salva(usuario);
+        log.info("[final] UsuarioAplicationService - alteraUsuarioPeloId");
     }
 }
