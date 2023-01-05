02e0f661a159dee51dd7307f7d9db339491c44cc
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-01-05 17:42:21 -0300

    implementa os metodos  buscaUsuarioPorId, buscaTodosUsuarios e deleta

--- src/main/java/br/com/nativos/produtivos/usuario/infra/UsuarioInfraRepository.java
+++ src/main/java/br/com/nativos/produtivos/usuario/infra/UsuarioInfraRepository.java
@@ -1,10 +1,15 @@
 package br.com.nativos.produtivos.usuario.infra;
 
+import br.com.nativos.produtivos.handler.APIException;
 import br.com.nativos.produtivos.usuario.aplication.service.UsuarioRepository;
 import br.com.nativos.produtivos.usuario.domain.Usuario;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,5 +23,28 @@ public class UsuarioInfraRepository implements UsuarioRepository {
         usuarioSpringDataJPARepository.save(usuario);
         log.info("[finaliza] UsuarioInfraRepository - salva");
         return usuario;
+    }
+    @Override
+    public Usuario buscaUsuarioPorId(UUID idUsuario) {
+        log.info("[inicia] UsuarioInfraRepository - buscaUsuarioPorId");
+        Usuario buscaUsuario = usuarioSpringDataJPARepository.findById(idUsuario)
+                .orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
+        log.info("[final] UsuarioInfraRepository - buscaUsuarioPorId");
+        return buscaUsuario;
+    }
+    @Override
+    public List<Usuario> buscaTodosUsuarios() {
+        log.info("[inicia] UsuarioInfraRepository - buscaTodosUsuarios");
+        List<Usuario> buscaUsuarios = usuarioSpringDataJPARepository.findAll();
+        log.info("[finaliza] UsuarioInfraRepository - buscaTodosUsuarios");
+        return buscaUsuarios;
+    }
+
+    @Override
+    public void deleta(UUID idUsuario) {
+        log.info("[inicia] UsuarioInfraRepository - deleta");
+        log.info("[IdUsuario]", idUsuario);
+        usuarioSpringDataJPARepository.deleteById(idUsuario);
+        log.info("[final] UsuarioInfraRepository - deleta");
     }
 }
