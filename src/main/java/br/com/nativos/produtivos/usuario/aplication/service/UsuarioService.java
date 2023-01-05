811468b830192213298242394ffb5a74886d0925
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-01-05 17:46:54 -0300

    implementa os metodo  buscaTodosUsuarios,alteraUsuarioPeloId e  altera o metodo buscaUsuarioPorId

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioService.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioService.java
@@ -1,12 +1,15 @@
 package br.com.nativos.produtivos.usuario.aplication.service;
 
-import br.com.nativos.produtivos.usuario.aplication.api.UsuarioCriadoResponse;
-import br.com.nativos.produtivos.usuario.aplication.api.UsuarioNovoRequest;
+import br.com.nativos.produtivos.usuario.aplication.api.*;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UsuarioService {
     UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
     void deletaUsuarioUsandoId(UUID idUsuario);
-    UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario);
+    UsuarioCriadoDetalhado buscaUsuarioPorId(UUID idUsuario);
+    List<UsuarioListResponse> buscaTodosUsuarios();
+    void alteraUsuarioPeloId(UUID idUsuario, UsuarioAlteracaoRequest usuarioAlteracaoRequest);
+
 }
