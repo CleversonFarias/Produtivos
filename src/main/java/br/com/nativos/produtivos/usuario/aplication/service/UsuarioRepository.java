0a33f23131ea077f3520d344ff1f033acbd06c4e
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-01-05 17:45:15 -0300

    implementa o metodo  buscaTodosUsuarios e altera metodo buscaUsuarioPorId

--- src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioRepository.java
+++ src/main/java/br/com/nativos/produtivos/usuario/aplication/service/UsuarioRepository.java
@@ -1,12 +1,13 @@
 package br.com.nativos.produtivos.usuario.aplication.service;
 
-import br.com.nativos.produtivos.usuario.aplication.api.UsuarioCriadoResponse;
 import br.com.nativos.produtivos.usuario.domain.Usuario;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UsuarioRepository {
     Usuario salva(Usuario usuario);
     void deleta(UUID idUsuario);
-    UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario);
+    Usuario buscaUsuarioPorId(UUID idUsuario);
+    List<Usuario> buscaTodosUsuarios();
 }
